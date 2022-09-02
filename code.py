308f98093713b2eff148d78dc936e16747d528d8
Artem Munaev <artvik0121@gmail.com>
2022-09-02 14:32:01 +0300

    code v1

--- code.py
+++ code.py
@@ -1,14 +1,16 @@
 #only with even nambers
-input = 4.8
+input = 0
 if input > 0:
-    hoper = input % 2
+    hoper = float(input) - int(input)
     if hoper < 0.5:
         print(int(input))
     else:
         print(int(input + 1))
 elif input < 0:
-    hoper1 = (input - 2 * input) % 2
-    if hoper1 < 0.5:
+    hoper1 = float(input) - int(input)
+    if hoper1 > -0.5:
         print(int(input))
     else:
         print(int(input - 1))
+else:
+    print(0)
